147147e2cd607740bd877bd6fe0d9594999f14dd
omkar14993 <omkarsalunke14993@gmail.com>
2018-05-18 12:38:38 -0400

    compiled java program and modified run and test script

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ if [[ $1 ]]
 then
    if [[ -e "$1" ]]
    then
-       edit-me-to-run-your-program "$1"
+       javac App.java
+       java App $1 $2 $3
    else
        echo "Template not found: $1" >&2
    fi
