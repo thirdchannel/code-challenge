2c68c32f9ab36b8cb0332e28f683056530824bee
Dominick <dominick@finnest.co>
2019-06-24 21:56:08 -0400

    completed coding challenge

--- run.sh
+++ run.sh
@@ -4,7 +4,31 @@ if [[ $1 ]]
 then
    if [[ -e "$1" ]]
    then
-       edit-me-to-run-your-program "$1"
+    declare -a variableArray
+    counter=0
+    while read f
+      do
+        stringLength=$(echo -n "input/$f" | wc -m)
+        if [ $stringLength != 10 ]; then
+          variableArray[counter]=$(sed -e 's#.*=\(\)#\1#' <<< "input/$f")
+          counter=`expr $counter + 1`
+        fi
+      done
+      
+    if [[ ${#variableArray[@]} >  4 ]]; then
+        { cat $1; cat; } | while read line
+       do
+        if [[ $line == *"((name))"* ]]; then
+          echo $(sed "s/((name))/${variableArray[0]}/g" <<< $line)
+        elif [[ $line == *"((product))"* ]]; then
+          echo $(sed -e "s/((product))/${variableArray[1]}/g" -e "s/((gift))/${variableArray[2]}/g" -e "s/((gift-value))/${variableArray[3]}/g" <<< $line)
+        elif [[ $line == *"((representative))"* ]]; then
+             echo $(sed "s/((representative))/${variableArray[4]}/g" <<< $line)
+        else
+          echo $line
+        fi
+      done
+    fi
    else
        echo "Template not found: $1" >&2
    fi
