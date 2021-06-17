6aca472b26ec339b2ad7b101960f8d916a8791cc
walterswoodward <walter.sekope.woodward@gmail.com>
2021-06-16 17:38:29 -0700

    Updates submission to pass tests - Reads STDIN directly from intial command rather than through a separate prompt - Refactors how str_replace is called to improve performance - Adds some comments for (hopefully) easier reading

--- generateMessage.php
+++ generateMessage.php
@@ -1,29 +1,27 @@
 <?php
+
 // 1. Parse a template file, specified as the first command line argument, into an in-memory representation.
 $content = file_get_contents('template');
 
 // 2. Read a data file containing variable bindings from STDIN, parsing it into an in-memory representation.
-$line = readline();
-
-if (!file_exists($line)) {
-    exit();
-}
-$dataFile = file_get_contents($line);
-
-// 3. Apply the data to the template.
 $lines = array_filter(explode("\n", $dataFile));
-foreach($lines as $line) {
-    $key = trim(substr($line, 0, strpos($line, '=')));
+$templateDict = [];
+while($line = fgets(STDIN)){
+    $key = trim(substr($line, 0, strpos($line, '='))); // Leading or trailing whitespace in variable names and values stripped.
     $value = trim(substr($line,  strpos($line, '=') + 1, -1));
-    $content = str_replace("((".$key."))", $value, $content);
-};
+    $templateDict[$key] = $value;
+}
 
-// Check for remaining double parens, indicating some template variables were not supplied
+// 3. Utilize str_replace's array shorthand to replace placeholders
+$replaceThese = array_map(function($key){return "((".$key."))";}, array_keys($templateDict));
+$withThese = array_values($templateDict);
+$content = str_replace($replaceThese, $withThese, $content);
+
+// 4. If any variable placeholders in the template remain (e.g. were not supplied) exit without output
 preg_match_all("/\(\(.*\)\)/", $content, $matches);
 if (!empty($matches[0])) {
     exit();
 }
 
-// 4. Output the result to STDOUT.
+// 5. Output the result to STDOUT
 fwrite(STDOUT, $content);
-
