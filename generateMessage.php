73442951d14a9b819a162d911dcd1dcb716cf803
walterswoodward <walter.sekope.woodward@gmail.com>
2021-06-20 13:44:02 -0700

    Replaces hardcoded ref to 'template' file with ref to 2nd element in argv - Renames some variables in generateMessage.php

--- generateMessage.php
+++ generateMessage.php
@@ -1,27 +1,27 @@
 <?php
 
 // 1. Parse a template file, specified as the first command line argument, into an in-memory representation.
-$content = file_get_contents('template');
+$template = file_get_contents($argv[1]);
 
 // 2. Read a data file containing variable bindings from STDIN, parsing it into an in-memory representation.
 $lines = array_filter(explode("\n", $dataFile));
-$templateDict = [];
+$dict = [];
 while($line = fgets(STDIN)){
     $key = trim(substr($line, 0, strpos($line, '='))); // Leading or trailing whitespace in variable names and values stripped.
     $value = trim(substr($line,  strpos($line, '=') + 1, -1));
-    $templateDict[$key] = $value;
+    $dict[$key] = $value;
 }
 
 // 3. Utilize str_replace's array shorthand to replace placeholders
-$replaceThese = array_map(function($key){return "((".$key."))";}, array_keys($templateDict));
-$withThese = array_values($templateDict);
-$content = str_replace($replaceThese, $withThese, $content);
+$replaceThese = array_map(function($key){return "((".$key."))";}, array_keys($dict));
+$withThese = array_values($dict);
+$template = str_replace($replaceThese, $withThese, $template);
 
 // 4. If any variable placeholders in the template remain (e.g. were not supplied) exit without output
-preg_match_all("/\(\(.*\)\)/", $content, $matches);
+preg_match_all("/\(\(.*\)\)/", $template, $matches);
 if (!empty($matches[0])) {
     exit();
 }
 
 // 5. Output the result to STDOUT
-fwrite(STDOUT, $content);
+fwrite(STDOUT, $template);
